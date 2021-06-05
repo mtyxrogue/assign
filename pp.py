9340768e4b1a3834919a2346871c1b19a638336d
mtyxrogue <saraths289@gmail.com>
2021-06-04 22:50:48 -0700

    assign

---
+++ pp.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import streamlit as st 
+import seaborn as sns 
+import matplotlib.pyplot as plt
+dr=pd.read_csv('rp.csv')
+dd=pd.DataFrame(dr)
+st.header("DataFrame")
+st.write(dd)
+rr=dd[["Country name","year","Social support","Freedom to make life choices","Positive affect","Life Ladder"]]
+st.write(rr)
+st.header("Correlation")
+corr=rr.corr()
+st.write(corr)
+s=sns.heatmap(corr)
+st.pyplot()
+st.line_chart(data=corr)
+
+
+
